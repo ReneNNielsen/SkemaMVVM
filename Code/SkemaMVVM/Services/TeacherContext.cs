641aa0ecc87c3ccd43b5dc9b00eb812e4c7a05a0
PingPongLee <djda9l@gmail.com>
2015-11-13 10:02:30 +0100

    Entity Framework integration preparation

---
+++ Code/SkemaMVVM/Services/TeacherContext.cs
@@ -0,0 +1,36 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class TeacherContext : DataContext
+    {
+        DataContext context;
+
+        public TeacherContext()
+        {
+            context = new DataContext();
+        }
+
+        public DataContext DataContext
+        {
+            get { return context; }
+        }
+
+        public bool AddNewTeacher(string name)
+        {
+            Teacher newTeacher = new Teacher()
+            {
+                FirstName = name
+            };
+
+            context.Teachers.Add(newTeacher);
+
+            return true;
+        }
+    }
+}

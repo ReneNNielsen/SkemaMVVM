5b5266cc5244ff1c662e6c31a2a825314568355a
ReneNNielsen <renennielsen@gmail.com>
2015-11-15 16:39:38 +0100

    Messages added

--- Code/SkemaMVVM/Messengers/Message.cs
+++ Code/SkemaMVVM/Messengers/Message.cs
@@ -8,5 +8,36 @@ namespace Messengers
 {
     public class Message
     {
+        public Message(MessageTypes messageType)
+        {
+            this.messageType = messageType;
+        }
+        /// <summary>
+        /// Has the message been handled
+        /// </summary>
+        public MessageHandledStatus HandledStatus
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// What type of message is this
+        /// </summary>
+        private MessageTypes messageType;
+        public MessageTypes MessageType
+        {
+            get
+            {
+                return messageType;
+            }
+        }
+        /// <summary>
+        /// The payload for the message
+        /// </summary>
+        public object Payload
+        {
+            get;
+            set;
+        }
     }
 }
---
+++ Code/SkemaMVVM/Messengers/MessageTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messengers
+{
+    public enum MessageTypes
+    {
+        MSG_Teacher_SELECTED_FOR_EDIT,// Sent when a Teacher is selected for editing
+        MSG_Teacher_SAVED      // Sent when a Teacher is updated to the repository
+    };
+}
